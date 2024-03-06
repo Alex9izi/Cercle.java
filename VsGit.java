f6dae7cb475bed86f84410e37a568d2f3b4a7c5e
Alexandre <150161151+Alex9izi@users.noreply.github.com>
2024-03-06 15:15:19 +0100

    teste py

--- VsGit.java
+++ VsGit.java
@@ -1,1 +1,11 @@
-sdfdsffbljqndsbkmldsbnmzskjdlbgkjsqmdlkjvlsqkjdvknqdknvlsjv:kdjfbkjmlqkdlqjzs
+publicfinalclassMath{
+    ...
+    
+    public static final double PI=3.14159265358979323846;
+    ...
+    
+    public static double toRadians(doubleangdeg){
+        return angdeg / 180.0 * PI;
+    }
+    ...
+}
